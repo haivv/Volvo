22c014fbb56771942c6c5ef894df82e203d88f07
VuVanHai <haivv279@gmail.com>
2023-07-18 17:54:20 +0900

    07-18

--- routes/notice.js
+++ routes/notice.js
@@ -3,12 +3,13 @@ var router = express.Router();
 const IP = require('ip'); //get client ip
 var database = require('../database');
 const fs = require('fs');
+var multer = require('multer');
 
 /* GET home page. */
 
 
 router.get('/', function (req, res, next) {
-   var ses =  req.session.sesWriter;
+    var ses = req.session.sesWriter;
     var page = 1;
     var sql = "SELECT * FROM notice";
     // res.send(sql);
@@ -41,15 +42,15 @@ router.get('/', function (req, res, next) {
 
 router.get('/getWriter/:writer', function (req, res, next) {
     var writer = req.params.writer;
-    
+
     req.session.sesWriter = writer;
     //var username = req.session.sesWriter;
-   // res.send(``);
+    // res.send(``);
     res.redirect('/notice/updateComment');
-   // res.send(req.session.sesWriter)
+    // res.send(req.session.sesWriter)
     // var sesWriter = req.session.sesWriter;
-   // res.redirect('/notice?param=${sesWriter}');
-   // res.send(req.session.sesWriter);
+    // res.redirect('/notice?param=${sesWriter}');
+    // res.send(req.session.sesWriter);
     // Data
     // const data = {
     //   name: writer,
@@ -64,78 +65,121 @@ router.get('/getWriter/:writer', function (req, res, next) {
     //     console.log('Added to JSON.');
     //   }
     // });
-   
-  });
-  
 
-  
+});
+
+
+
 router.get('/notice_add', function (req, res, next) {
     // var writer = req.session.sesWriter;
     // res.send(writer);
-   res.render('notice_add', { title: 'Notice'});  
+    res.render('notice_add', { title: 'Notice' });
 });
+
+// Upload file
+// // SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+        //add datetime to file name
+        var date_ob = new Date();
+        var day = ("0" + date_ob.getDate()).slice(-2);
+        var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+        var year = date_ob.getFullYear();
+        var date = year + "-" + month + "-" + day;
+        var hours = date_ob.getHours();
+        var minutes = date_ob.getMinutes();
+        var seconds = date_ob.getSeconds();
+        var dateTime = year + "_" + month + "_" + day + "_" + hours + "_" + minutes + "_" + seconds;
+
+        var uniqueFileName = dateTime+'_'+file.originalname;
+
+
+        callback(null, uniqueFileName);
+    }
+});
+
+
+var upload = multer({ storage: storage }).single('myfile');
+
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+//upload file
+router.post('/uploadfile', (req, res) => {
+   
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Error uploading file.");
+        }
+        console.log("File is uploaded successfully!");
+    });
+
+
+});
+
+
+
 
 router.post('/proAddNotice', function (req, res, next) {
 
     const txtSelect = req.body.searchoption;
-    var txtOption ='';
-    if(txtSelect =='op1'){
+    var txtOption = '';
+    if (txtSelect == 'op1') {
         txtOption = "Announcement";
-    } else if(txtSelect =='op2'){
+    } else if (txtSelect == 'op2') {
         txtOption = "Suggestion";
-    }else if(txtSelect =='op3'){
+    } else if (txtSelect == 'op3') {
         txtOption = "Opinion";
-    }else if(txtSelect =='op4'){
+    } else if (txtSelect == 'op4') {
         txtOption = "Request";
-    }else if(txtSelect =='op5'){
+    } else if (txtSelect == 'op5') {
         txtOption = "Reference";
     }
-    
-    
-    // fs.readFile('user.json', (err, data) => {
-    //     if (err) {
-    //       console.error('Error reading file:', err);
-    //       res.status(500).send('Error reading data file');
-    //     } else {
-        //   var jsonData = JSON.parse(data);
-        //   var name = jsonData.name;
-          const txtTitle = req.body.txtTitle;
-          var txtContent = req.body.txtContent;
-          var newContent = database.escape(txtContent);
-          const txtWrite = req.session.sesWriter;
-          
-          var today = new Date();
-      
-          var year = today.getFullYear();
-          var month = ('0' + (today.getMonth() + 1)).slice(-2);
-          var day = ('0' + today.getDate()).slice(-2);
-      
-          var dateString = year + '-' + month  + '-' + day;
-      
-          var query = `
+
+
+    var txtTitle = req.body.txtTitle;
+    console.log(txtTitle);
+    var txtContent = req.body.txtContent;
+    var newContent = database.escape(txtContent);
+    var txtWrite = req.session.sesWriter;
+
+    var today = new Date();
+
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+
+    var dateString = year + '-' + month + '-' + day;
+
+
+    var fileUpload = req.body.txtfileupload;
+
+
+    var query = `
           INSERT INTO notice 
-          (title,category, dateCreate, writer, content) 
-          VALUES ("${txtTitle}", "${txtOption}", "${dateString}", "${txtWrite}", "${newContent}")
+          (title,category, dateCreate, writer, content, fileUpload) 
+          VALUES ("${txtTitle}", "${txtOption}", "${dateString}", "${txtWrite}", "${newContent}", "${fileUpload}")
           `;
-      
-          database.query(query, function(error, data){
-      
-              if(error)
-              {
-                  throw error;
-              }	
-              else
-              {
-                  res.render('mess', { title: 'Add notice success!', sess:req.session.sesWriter});
-              }
-      
-          });
-       
-       
-       
-    //     }//end fs read json
-    //   });
-    
+
+    database.query(query, function (error, data) {
+
+        if (error) {
+            throw error;
+        }
+        else {
+            res.render('mess', { title: 'Add notice success!', sess: req.session.sesWriter });
+        }
+
+    });
+
+
+
+
+
 });
 
 
@@ -143,130 +187,127 @@ router.get('/comment/:key', function (req, res, next) {
     var id = req.params.key;
     var query = `SELECT * FROM notice WHERE id = "${id}"`;
 
-	database.query(query, function(error, data){
+    database.query(query, function (error, data) {
         var queryComment = `SELECT * FROM comment WHERE idNotice = "${id}" ORDER BY id DESC `;
-        
-        database.query(queryComment, function(error,data2){
-            res.render('comment', {title: 'Notice', dataPage:data, dataComment:data2});
+
+        database.query(queryComment, function (error, data2) {
+            res.render('comment', { title: 'Notice', dataPage: data, dataComment: data2 });
         });
-       
 
-		
-	});
-   
- });
 
- router.post('/addcomment', function (req, res, next) {
+
+    });
+
+});
+
+router.post('/addcomment', function (req, res, next) {
     //fs.readFile('user.json', (err, data) => {
-        // if (err) {
-        //   console.error('Error reading file:', err);
-        //   res.status(500).send('Error reading data file');
-        // } else {
-        //     var jsonData = JSON.parse(data);
-        //   var name = jsonData.name;
-            var idNotice = req.body.idNotice;
-            var comWriter = req.session.sesWriter;
-            var txtTypeComment = req.body.txtTypeComment;
-            
+    // if (err) {
+    //   console.error('Error reading file:', err);
+    //   res.status(500).send('Error reading data file');
+    // } else {
+    //     var jsonData = JSON.parse(data);
+    //   var name = jsonData.name;
+    var idNotice = req.body.idNotice;
+    var comWriter = req.session.sesWriter;
+    var txtTypeComment = req.body.txtTypeComment;
 
-            var today = new Date();
-            var year = today.getFullYear();
-            var month = ('0' + (today.getMonth() + 1)).slice(-2);
-            var day = ('0' + today.getDate()).slice(-2);
 
-            var dateString = year + '-' + month  + '-' + day;
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
 
-            //res.send(idNotice);
-            var query = `
+    var dateString = year + '-' + month + '-' + day;
+
+    //res.send(idNotice);
+    var query = `
             INSERT INTO comment 
             (idNotice ,comWriter, comDate, comContent) 
             VALUES ("${idNotice}", "${comWriter}", "${dateString}", "${txtTypeComment}")
             `;
 
-            database.query(query, function(error, data){
+    database.query(query, function (error, data) {
 
-                if(error)
-                {
-                    throw error;
-                }	
-                else
-                {
-                    res.redirect('/notice/comment/'+idNotice);
-                // res.send("pl");
-                }
+        if (error) {
+            throw error;
+        }
+        else {
+            res.redirect('/notice/comment/' + idNotice);
+            // res.send("pl");
+        }
 
-            });
+    });
 
 
 
     //          }//end fs read json
     //   });
- 
-   
- });
-router.get('/updateComment', function (req, res, next) {
-    
-   // var sql = "SELECT idNotice FROM comment GROUP by(idNotice) ";
-const columnName = 'id';
-///get id from main table
-  database.query(`SELECT ${columnName} FROM notice`, (error, results, fields) => {
-    if (error) {
-      console.error('Error executing the query:', error);
-      return res.status(500).send('An error occurred');
-    }
-
-    // Extract the column values from the result set
-    const  columnValues = results.map((row) => row[columnName]);
-   
-    
-   
-    for (let i = 0; i< columnValues.length; i++)
-    {
-        console.log(columnValues[i]);
-
-        //count number of commment (foreign table)
-        database.query(`SELECT COUNT(*) AS count FROM comment where idNotice = ${columnValues[i]}`, (error, results2, fields2) => {
-            if (error) {
-              console.error('Error executing the query:', error);
-              return;
-            }
-          
-            // Access the count value from the result
-            const count = results2[0].count;
-            console.log('Record count:', count);
-
-            //update number of comment
-            database.query(`UPDATE notice SET comment = ${count} where id =${columnValues[i]}`, (error, results2, fields2) => {
-                if (error) {
-                  console.error('Error executing the query:', error);
-                  return;
-                }
-              
-                console.log('Updated');
-    
-                // Close the connection
-                
-    
-    
-    
-    
-    
-              });
-            
 
 
-
-
-
-          });
-
-
-
-
-    }//end for
-  
-    res.redirect('/notice');
 });
+router.get('/updateComment', function (req, res, next) {
+
+    // var sql = "SELECT idNotice FROM comment GROUP by(idNotice) ";
+    const columnName = 'id';
+    ///get id from main table
+    database.query(`SELECT ${columnName} FROM notice`, (error, results, fields) => {
+        if (error) {
+            console.error('Error executing the query:', error);
+            return res.status(500).send('An error occurred');
+        }
+
+        // Extract the column values from the result set
+        const columnValues = results.map((row) => row[columnName]);
+
+
+
+        for (let i = 0; i < columnValues.length; i++) {
+            console.log(columnValues[i]);
+
+            //count number of commment (foreign table)
+            database.query(`SELECT COUNT(*) AS count FROM comment where idNotice = ${columnValues[i]}`, (error, results2, fields2) => {
+                if (error) {
+                    console.error('Error executing the query:', error);
+                    return;
+                }
+
+                // Access the count value from the result
+                const count = results2[0].count;
+                console.log('Record count:', count);
+
+                //update number of comment
+                database.query(`UPDATE notice SET comment = ${count} where id =${columnValues[i]}`, (error, results2, fields2) => {
+                    if (error) {
+                        console.error('Error executing the query:', error);
+                        return;
+                    }
+
+                    console.log('Updated');
+
+                    // Close the connection
+
+
+
+
+
+
+                });
+
+
+
+
+
+
+            });
+
+
+
+
+        }//end for
+
+        res.redirect('/notice');
+    });
 
 
 });
