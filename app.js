89e53e633579ffa0522cb177f57528beb3a46fa2
VuVanHai <haivv279@gmail.com>
2023-07-28 10:07:34 +0900

    07-28

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use('/users', usersRouter);
 // app.use('/sort', sortRouter);
 app.use('/unity', unityRouter);
 app.use('/notice', noticeRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -60,8 +61,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.get('*', (req, res) => {
-  res.status(404).send('Page not found ^^');
-});
+
 
 module.exports = app;
